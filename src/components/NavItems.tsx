d710740ffc341395cdbe0ab56c53dd21ae2fa5aa
Rene Pungartnik <rene.pungartnik98@gmail.com>
2023-11-27 09:50:53 +0100

    added click outside hook and esc button function for closing navbar - user friendly

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,16 +1,35 @@
 "use client";
 import { PRODUCT_CATEGORIES } from "@/config";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import NavItem from "./NavItem";
+import { useOnClickOutside } from "@/hooks/use-on-click-outside";
 //*************************************IMPORTS*************************************
 const NavItems = () => {
     const [activeIndex, setActiveIndex] = useState<null | number>(null)
 
+    useEffect(() => {
+        const handler = (e: KeyboardEvent) => {
+            if (e.key === "Escape") {
+                setActiveIndex(null) // closing navbar with esc button
+            }
+        }
+
+        document.addEventListener("keydown", handler)
+
+        return () => {
+            document.removeEventListener("keydown", handler)
+        }
+    }, [])
+
     const isAnyOpen = activeIndex !== null
+
+    const navRef = useRef<HTMLDivElement | null>(null)
+
+    useOnClickOutside(navRef, () => setActiveIndex(null)) // closing navbar with clicking outside of the navbar
 
 
     return (
-        <div className="flex gap-4 h-full">
+        <div className="flex gap-4 h-full" ref={navRef}>
             {PRODUCT_CATEGORIES.map((category, i) => {
 
                 const handleOpen = () => {
