1cf16b3b8ccf3043134072d3d03df78cce78a616
Rene Pungartnik <rene.pungartnik98@gmail.com>
2023-11-30 18:13:53 +0100

    added toaster messages upon successful or unsuccessful sign up

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { Icons } from "@/components/Icons";
 import { Button, buttonVariants } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
@@ -8,9 +9,11 @@ import { ArrowRight } from "lucide-react";
 import Link from "next/link";
 import {useForm} from "react-hook-form";
 import {zodResolver} from "@hookform/resolvers/zod";
-import {z} from "zod";
+import {ZodError, z} from "zod";
 import { AuthCredentialsValidator, TAuthCredentialsValidator } from "@/lib/validators/account-credentials-validator";
 import { trpc } from "@/trpc/client";
+import {toast} from "sonner";
+import { useRouter } from "next/navigation";
 //******************************************IMPORTS*********************************************
 
 const Page = () => {
@@ -18,11 +21,34 @@ const Page = () => {
     const { register, handleSubmit, formState: { errors }, } = useForm<TAuthCredentialsValidator>({
         resolver: zodResolver(AuthCredentialsValidator),
     })
+//-----------------------------------------------------------------------------------------------------
+
+    const router = useRouter()
 
     const {mutate, isLoading} = trpc.auth.createPayloadUser.useMutation({
+        onError: (err) => {
+            if(err.data?.code === "CONFLICT") {
+                toast.error(
+                    "This email is already in use. Sign in instead?"
+                )
 
+                return
+            }
+
+            if(err instanceof ZodError) {
+                toast.error(err.issues[0].message)
+
+                return
+            }
+
+            toast.error("Something went wrong. Please try again!")
+        },
+        onSuccess: ({sentToEmail}) => {
+            toast.success(`Verification email sent to ${sentToEmail}.`)
+            router.push("/verify-email?to=" + sentToEmail)
+        }
     })
-
+//-----------------------------------------------------------------------------------------------------
     const onSubmit = ({email, password}: TAuthCredentialsValidator) => {
         mutate({ email, password })
     }
