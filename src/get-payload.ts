5a56b2c7ddf74e5e6ecc2d52b67ef29cbab217b0
Rene Pungartnik <rene.pungartnik98@gmail.com>
2023-11-30 10:28:57 +0100

    configuring resend email confirmation - works

--- src/get-payload.ts
+++ src/get-payload.ts
@@ -2,12 +2,24 @@ import dotenv from "dotenv";
 import path from "path";
 import type { InitOptions } from "payload/config";
 import payload, { Payload } from "payload";
+import nodemailer from "nodemailer";
 //********************IMPORTS********************
 //___________________________payload client for a database________________________________________________
 // part of server.ts file!
 
 dotenv.config({
-    path: path.resolve(__dirname, "../.env")
+    path: path.resolve(__dirname, "../.env"),
+})
+
+// defining transporter for our email*, configured to send emails using the SMTP server hosted at "smtp.resend.com"
+const transporter = nodemailer.createTransport({
+    host: "smtp.resend.com",
+    secure: true,
+    port: 465, // standard port for email
+    auth: {
+        user: "resend",
+        pass: process.env.RESEND_API_KEY
+    }
 })
 
 let cached = (global as any).payload
@@ -36,6 +48,11 @@ export const getPayloadClient = async ({
 
     if(!cached.promise) {
         cached.promise = payload.init({
+            email: {
+                transport: transporter,
+                fromAddress: "onboarding@resend.dev",
+                fromName: "HippoHub",
+            },
             secret: process.env.PAYLOAD_SECRET,
             local: initOptions?.express ? false : true,
             ...(initOptions || {}),
