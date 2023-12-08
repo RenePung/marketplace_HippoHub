6b3a1d642b0eef639ae8105cb7817c9bead55686
Rene Pungartnik <rene.pungartnik98@gmail.com>
2023-12-09 00:06:07 +0100

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -46,7 +46,7 @@ export default function Home() {
           </div>
       </div>
 
-      <ProductReel title="Brand new" />
+      <ProductReel query={{sort: "desc", limit: 4}} href="/products" title="Brand new" />
     </MaxWidthWrapper>
 
     <section className="border-t border-gray-200 bg-gray-50">
--- src/components/ProductReel.tsx
+++ src/components/ProductReel.tsx
@@ -1,12 +1,29 @@
+"use client"
+
+import { TQueryValidator } from "@/lib/validators/query-validators"
+import { trpc } from "@/trpc/client"
+import Link from "next/link"
+
 interface ProductReelProps {
     title: string
     subtitle?: string
+    href?: string
+    query: TQueryValidator
 }
 
+const FALLBACK_LIMIT = 4
 
 const ProductReel = (props: ProductReelProps) => {
-    
-    const {title, subtitle} = props
+    const {title, subtitle, href, query} = props
+
+    const {data} = trpc.getInfiniteProducts.useInfiniteQuery({
+        limit: query.limit ?? FALLBACK_LIMIT, query
+    }, 
+    {
+        getNextPageParam: (lastPage) => lastPage.nextPage
+    })
+
+    console.log("data", data)
 
     return (
         <section className="py-12">
@@ -22,6 +39,21 @@ const ProductReel = (props: ProductReelProps) => {
                         {subtitle}
                     </p> 
                     ) : null}
+                </div>
+
+                {href ? (
+                    <Link href={href} className="hidden text-sm font-medium text-violet-600 hover:text-violet-500 md:block">
+                        Shop the collection{' '} 
+                        <span aria-hidden= "true">&rarr;</span>
+                    </Link> 
+                ) : null}
+            </div>
+
+            <div className="relative">
+                <div className="mt-6 flex items-center w-full">
+                    <div className="w-full grid grid-cols-2 gap-x-4 gap-y-10 sm:gap-x-6 md:grid-cols-4 md:gap-y-10 lg:gap-x-8">
+
+                    </div>
                 </div>
             </div>
         </section>
