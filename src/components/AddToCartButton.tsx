aeb7c2cb9331c41a3e2fd70d37974f67c51a2056
Rene Pungartnik <rene.pungartnik98@gmail.com>
2023-12-11 18:11:08 +0100

    added cart logic

--- src/components/AddToCartButton.tsx
+++ src/components/AddToCartButton.tsx
@@ -2,9 +2,11 @@
 
 import { useEffect, useState } from "react"
 import { Button } from "./ui/button"
-
-const AddToCartButton = () => {
-
+import { useCart } from "@/hooks/use-cart"
+import { Product } from "@/payload-types"
+//--------------------------------------------------------------------------------------------------------------
+const AddToCartButton = ({product}: {product: Product}) => {
+    const {addItem} = useCart()
     const [isSuccess, setIsSuccess] = useState<boolean>(false)
 
     useEffect(() => {
@@ -15,9 +17,9 @@ const AddToCartButton = () => {
         return () => clearTimeout(timeout)
     }, [isSuccess])
 
-
     return (
         <Button onClick={() => {
+            addItem(product)
             setIsSuccess(true)
         }} size="lg" className="w-full">{isSuccess ? "Successfully added!" : "Add to cart"}</Button>
     )
